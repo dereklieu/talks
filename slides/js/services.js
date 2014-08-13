a525d5b2b52dc88673d74d556a7b40aa355cf20d
Derek Lieu <derek@developmentseed.org>
2014-08-13 09:46:56 -0400

    Minimal slides

--- slides/js/services.js
+++ slides/js/services.js
@@ -3,8 +3,23 @@
 angular.module('talkyApp.services', [])
 .service('slideData', [function() {
 
-    var slides = [
-    ];
+    var slides = [{
+        title: 'http://bit.ly/1AcvUdI'
+    },{
+        title: 'http://bit.ly/1AcwurY'
+    },{
+        imgUrl: 'img/populationchg.jpg'
+    },{
+        imgUrl: 'img/badpolitical.png'
+    },{
+        imgUrl: 'img/dc.jpg'
+    },{
+        imgUrl: 'img/metro.png'
+    },{
+        title: 'http://bit.ly/1l1yErg'
+    },{
+
+    }];
 
     return slides;
 }])
