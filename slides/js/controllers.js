e889a911d61d5215bf1b11481cf4cc24fdccbd39
Derek Lieu <derek@developmentseed.org>
2014-09-23 18:11:51 -0400

    First round of slides done

--- slides/js/controllers.js
+++ slides/js/controllers.js
@@ -5,13 +5,6 @@ angular.module('talkyApp.controllers', [])
 .controller('SlideCtrl', ['$scope', 'slideData', function($scope, slideData) {
     $scope.slides = slideData;
     $scope.length = $scope.slides.length;
-    $scope.current = 0;
+    $scope.current = slideData.length-1;
 }])
-
-.controller('TestCtrl', ['$scope', function($scope) {
-
-    console.log('in effect');
-
-}])
-
 ;
--- slides/js/services.js
+++ slides/js/services.js
@@ -4,6 +4,30 @@ angular.module('talkyApp.services', [])
 .service('slideData', [function() {
 
     var slides = [{
+        title: 'TOPO-what?'
+    },{
+        title: 'what-JSON?'
+    },{
+        title: 'what-end?',
+        decks: [
+            'Front-end is actually kind of a misnomer'
+        ]
+    },{
+        title: 'whatever-end',
+        decks: [
+            'But the complex stuff lives on the front-end'
+        ]
+    },{
+        title: 'whatever-it-takes--end',
+        decks: [
+            'This way kind of sucks',
+            'But other ways suck in different ways'
+        ],
+        cls: 'lightsalmon'
+    },{
+        title: 'way cool',
+        cls: 'waycool'
+    },{
 
     }];
 
